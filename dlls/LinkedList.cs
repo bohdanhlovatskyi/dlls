7493994a671269d86320c0b038dc37f9ab9468b3
bohdanhlovatskyi <bohdan.hlovatskyi@ucu.edu.ua>
2022-04-05 12:23:15 +0300

    added inserting and deleting

--- dlls/LinkedList.cs
+++ dlls/LinkedList.cs
@@ -1,11 +1,12 @@
 using System;
 using System.IO; 
-using System.Collections.Generic;
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace LinkedList {
 
-    class Node<T> {
+    public class Node<T> {
         public Node<T> next { get; set; }
         public Node<T> prev { get; set; }
         public T data { get; set; }
@@ -15,12 +16,18 @@ namespace LinkedList {
             prev = null;
             data = val;
         }
+
+        public override string ToString()
+        {
+            return $"{{{data}}}";
+        }
     }
 
-    // TODO: ask whether this can be incapsulated in LLIterator as it was done in C++
-    public class LinkedList<T> : IEnumerable<T> {    
+    public class LinkedList<T> : IEnumerable<Node<T>> {    
         private Node<T> head;
         private Node<T> tail;
+
+        private Node<T> current;
 
         private int itemCount = 0;
 
@@ -35,10 +42,10 @@ namespace LinkedList {
             tail = null;
         }
 
-        public IEnumerator<T> GetEnumerator() {
-            var current = head;
+        public IEnumerator<Node<T>> GetEnumerator() {
+            current = head;
             while (current != null) {
-                yield return current.data;
+                yield return current;
                 current = current.next;
             }
         }
@@ -61,6 +68,56 @@ namespace LinkedList {
                 head = node;
             }
             itemCount++;
+        }
+
+        public void Insert(T val, int pos) {
+            var nth = this.Skip(pos - 1).FirstOrDefault();
+            
+            InsertAfter(val, nth);
+        }
+
+
+        internal void InsertAfter(T val, Node<T> prev) {
+            if (prev == null) {
+                // throw new NullReferenceException("given prev is null ref");
+                return;
+            }
+
+            var node = new Node<T>(val);
+            node.next = prev.next;
+            prev.next = node;
+            node.prev = prev;
+            if (node.next != null) {
+                node.next.prev = node;
+            }
+
+            itemCount++;
+        } 
+
+        public Node<T> RemoveByKey(T val) {
+            var first = this.First<Node<T>>(node => node.data.Equals(val));
+            return RemoveNode(first);
+        }
+
+        public Node<T> Remove(int pos) {
+            var nth = this.Skip(pos).FirstOrDefault();
+            return RemoveNode(nth);
+        }
+
+        internal Node<T> RemoveNode(Node<T> node) {
+            if (node == null) {
+                return null;
+            }
+
+            if (node.next != null) {
+                node.next.prev = node.prev;
+            }
+
+            if (node.prev != null) {
+                node.prev.next = node.next;
+            }
+
+            return node;
         }
 
         public override string ToString()
--- dlls/Program.cs
+++ dlls/Program.cs
@@ -13,6 +13,10 @@ namespace dlls
             ll.PushFront(10);
             ll.PushFront(14);
             ll.PushFront(12);
+
+            ll.Insert(9, 1);
+            ll.Remove(2);
+
             Console.WriteLine(ll);
         }
     }
